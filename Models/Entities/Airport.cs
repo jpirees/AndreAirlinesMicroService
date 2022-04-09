94df8a0e3b7d5777bc00efd28c6ebcb164d21207
Junior Pires <jpirees@hotmail.com>
2022-04-09 16:42:53 -0300

    Adicionado Model para Aeroporto

---
+++ Models/Entities/Airport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Models.Entities
+{
+    public class Airport
+    {
+        public static readonly string GET_ALL = "SELECT Code, City, Country, Continent FROM Airport";
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Code")]
+        public string Code { get; set; }
+
+        [JsonProperty("City")]
+        public string City { get; set; }
+
+        [JsonProperty("Country")]
+        public string Country { get; set; }
+
+        [JsonProperty("Continent")]
+        public string Continent { get; set; }
+    }
+}
