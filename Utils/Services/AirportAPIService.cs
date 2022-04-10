ecf9426fbc8b2a31d1daa1a8abc52a949efbeca9
Junior Pires <jpirees@hotmail.com>
2022-04-10 13:04:42 -0300

    Adicionado classes de serviços para consultas na biblioteca utils.

---
+++ Utils/Services/AirportAPIService.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Models.Entities;
+using Newtonsoft.Json;
+
+namespace Utils.Services
+{
+    public class AirportAPIService
+    {
+        public static async Task<Airport> SearchByAirportId(int id)
+        {
+            HttpClient httpClient = new();
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:44334/api/Airports/{id}");
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                if (responseBody.ToString().Contains("erro"))
+                    return null;
+
+                var airplane = JsonConvert.DeserializeObject<Airport>(responseBody);
+
+                return airplane;
+            }
+            catch (HttpRequestException exception)
+            {
+                throw new HttpRequestException("Serviço indisponível", exception, System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        public static async Task<Airport> SearchByAirportCode(string code)
+        {
+            HttpClient httpClient = new();
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:44334/api/Airports/Code/{code}");
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                if (responseBody.ToString().Contains("erro"))
+                    return null;
+
+                var airplane = JsonConvert.DeserializeObject<Airport>(responseBody);
+
+                return airplane;
+            }
+            catch (HttpRequestException exception)
+            {
+                throw new HttpRequestException("Serviço indisponível", exception, System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
