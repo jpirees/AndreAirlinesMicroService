07c308648093093064c0f0849b9643422370aba2
Junior Pires <jpirees@hotmail.com>
2022-04-11 22:42:38 -0300

    Adicionado serviço para realizar consulta de usuário

---
+++ Utils/Services/UserAPIService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Entities;
+using Models.Entities.DTO;
+using Newtonsoft.Json;
+
+namespace Utils.Services
+{
+    public class UserAPIService
+    {
+        public static async Task<UserResponseDTO> SearchByUsername(string username, string password)
+        {
+            HttpClient httpClient = new();
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:44334/api/Users/{username}?password={password}");
+
+                if (!response.IsSuccessStatusCode)
+                    return null;
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                if (responseBody.ToString().Contains("erro"))
+                    return null;
+
+                var user = JsonConvert.DeserializeObject<UserResponseDTO>(responseBody);
+
+                //var user = new UserResponseDTO(userObject.Name, userObject.Username, userObject.Department, userObject.Role);
+
+                return user;
+            }
+            catch (HttpRequestException exception)
+            {
+                throw new HttpRequestException("Serviço indisponível", exception, System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+
+    }
+}
